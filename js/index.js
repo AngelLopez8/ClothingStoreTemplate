3bc1b6faaa68410d0fd8e88a31de7e01a98bf9a0
Angel Lopez <angelomarlopez10@gmail.com>
2022-08-30 20:44:35 -0500

    Final Responsive Commit

--- js/index.js
+++ js/index.js
@@ -3,3 +3,15 @@ let currentTab = document.querySelector(".current");
 currentTab.addEventListener("click", () => {
   currentTab.querySelector(".hidden-content").classList.toggle("open");
 });
+
+let hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", () => {
+  document.querySelector(".side-bar").classList.toggle("show");
+  // document.querySelector(".main").classList.toggle("idle");
+});
+
+let closeIcon = document.querySelector(".close");
+closeIcon.addEventListener("click", () => {
+  document.querySelector(".side-bar").classList.toggle("show");
+  // document.querySelector(".main").classList.toggle("idle");
+});
